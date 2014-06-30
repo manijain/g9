a1b6a4186eebe37cab44d82fe5a70074b59b5743
manish <manijain333@gmail.com>
2014-06-30 11:09:19 +0530

    add default url for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 G9::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'http://g9-builders.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
